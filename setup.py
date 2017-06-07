dd8df0abc4358fa0dfe880f46a6a8596786fe3de
Allen, Timothy <tallen@wharton.upenn.edu>
2017-06-07 18:59:01 +0000

    Bump version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='wagtailgridder',
-    version="0.9.9",
+    version="0.9.11",
     description='Gridder layout for the Django CMS Wagtail.',
     long_description='',
     author='Timothy Allen',
