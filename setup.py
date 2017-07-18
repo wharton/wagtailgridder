9d4f81ecaa8a77ccdea824472d5fd0a9e760313d
Allen, Timothy <tallen@wharton.upenn.edu>
2017-07-18 13:51:00 -0400

    Small CSS tweak to keep background images in expanded area from repeating.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='wagtailgridder',
-    version="0.9.14",
+    version="0.9.15",
     description='Gridder layout for the Django CMS Wagtail.',
     long_description='',
     author='Timothy Allen',
