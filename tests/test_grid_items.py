84fe47cb9b87d072f351d6a1c5280d32816e5274
Allen, Timothy <tallen@wharton.upenn.edu>
2024-09-18 11:57:08 -0400

    Get an initial test suite running.

--- tests/test_grid_items.py
+++ tests/test_grid_items.py
@@ -6,5 +6,7 @@ def test_create_page(grid_index_page):
     """
     Tests creating a Gridder page.
     """
-    print(grid_index_page.render())
-    assert False
+    assert grid_index_page.slug == "grid-index-page"
+    assert grid_index_page.featured_description == "Featured description"
+    assert grid_index_page.hero_button_text == "Hero button"
+    assert grid_index_page.grid_items[0].title == "TEST Grid Item"
